9457fe0a787377806959627ac8d8788e4a0ba77e
kemalelmizan <kemalelmizan@gmail.com>
2017-10-08 09:53:29 +0700

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+// Copyright (C) 2016-2017 Sergey Akopkokhyants
+// This project is licensed under the terms of the MIT license.
+// https://github.com/akserg
+
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {AppModule} from './app';
+
+platformBrowserDynamic().bootstrapModule(AppModule)
